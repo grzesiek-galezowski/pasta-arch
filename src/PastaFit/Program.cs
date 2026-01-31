56fe0a103f4a837fc105003a5e2e3bd9bc3108e4
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2026-01-31 23:15:42 +0100

    removed registrations

--- src/PastaFit/Program.cs
+++ src/PastaFit/Program.cs
@@ -1,17 +1,37 @@
 using PastaFit.Features.Booking.Adapters;
-using PastaFit.Features.Booking.Ports;
+using PastaFit.Features.Booking.CancelBooking;
+using PastaFit.Features.Booking.CreateBooking;
 using PastaFit.Shell.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
-var inMemoryBookingAdapter = new InMemoryBookingRepository();
-inMemoryBookingAdapter.Bootstrap();
-builder.Services.AddSingleton<ICreateBookingRepository>(inMemoryBookingAdapter);
-builder.Services.AddSingleton<ICancelBookingRepository>(inMemoryBookingAdapter);
-IServiceCollection temp = builder.Services;
+var repository = new InMemoryBookingRepository();
+repository.Bootstrap();
 
 var app = builder.Build();
 
-app.MapBookingEndpoints();
+app.MapPost(
+  "/bookings",
+  async (
+    BookingRequest request) =>
+  {
+    var responseInProgress = new CreateBookingResponseInProgress();
+    await CreateBookingHandler.Handle(request.MemberId, request.ClassId, repository, responseInProgress);
+    return responseInProgress.Result;
+  });
+
+app.MapDelete(
+  "/bookings/{bookingId:guid}",
+  async (
+    Guid bookingId) =>
+  {
+    var responseInProgress = new CancelBookingResponseInProgress();
+    await CancelBookingHandler.Handle(bookingId, repository, responseInProgress);
+    return responseInProgress.Result;
+  });
+
+app.MapGet("/classes", InMemoryBookingRepository.GetClassAvailability);
+
+app.MapGet("/members", InMemoryBookingRepository.GetAllMembers);
 
 app.Run();
 
--- src/PastaFit/Shell/Endpoints/BookingEndpoints.cs
+++ src/PastaFit/Shell/Endpoints/BookingEndpoints.cs
@@ -7,36 +7,6 @@ namespace PastaFit.Shell.Endpoints;
 
 public static class BookingEndpoints
 {
-  public static IEndpointRouteBuilder MapBookingEndpoints(this IEndpointRouteBuilder app)
-  {
-    app.MapPost(
-      "/bookings",
-      async (
-        BookingRequest request,
-        ICreateBookingRepository deps) =>
-      {
-        var responseInProgress = new CreateBookingResponseInProgress();
-        await CreateBookingHandler.Handle(request.MemberId, request.ClassId, deps, responseInProgress);
-        return responseInProgress.Result;
-      });
-
-    app.MapDelete(
-      "/bookings/{bookingId:guid}",
-      async (
-        Guid bookingId,
-        ICancelBookingRepository deps) =>
-      {
-        var responseInProgress = new CancelBookingResponseInProgress();
-        await CancelBookingHandler.Handle(bookingId, deps, responseInProgress);
-        return responseInProgress.Result;
-      });
-
-    app.MapGet("/classes", InMemoryBookingRepository.GetClassAvailability);
-
-    app.MapGet("/members", InMemoryBookingRepository.GetAllMembers);
-
-    return app;
-  }
 }
 
 public sealed record BookingRequest(Guid MemberId, Guid ClassId);
